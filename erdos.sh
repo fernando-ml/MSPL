9bb12b592095cdd09f4b4129ca5397cb9e842db8
Lesther Santana <lesthersan@gmail.com>
2025-06-29 22:35:18 -0400

    NB for dataviz

--- erdos.sh
+++ erdos.sh
@@ -1,7 +1,9 @@
-python main.py config_dl.yaml --dataset CICIoV2024 --mode prototypical
+python main.py test_config.yaml --dataset CICIoV2024 --mode prototypical
 
-python main.py config_dl.yaml --dataset CICEVSE_Network2024 --mode prototypical
+python main.py test_config.yaml --dataset CICEVSE_Network2024 --mode prototypical
 
-python main.py config_dl.yaml --dataset CICEVSE_PowerB2024 --mode prototypical
+python main.py test_config.yaml --dataset CICEVSE_PowerB2024 --mode prototypical
 
-python main.py config_dl.yaml --dataset CICIDS2017 --mode prototypical
+python main.py test_config.yaml --dataset CICIDS2017 --mode prototypical
+
+python main.py test_config_traditional.yaml --dataset CICIDS2017 --mode traditional
